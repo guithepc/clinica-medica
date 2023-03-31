25c2c6fe31ba1e5d26e89733bb5ac8e2da190109
Guilherme <guilherme.pc@yahoo.com>
2023-03-30 22:05:36 -0300

    adding entity config

---
+++ src/main/java/com/example/demo/medico/Medico.java
@@ -0,0 +1,28 @@
+package com.example.demo.medico;
+
+import com.example.demo.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
