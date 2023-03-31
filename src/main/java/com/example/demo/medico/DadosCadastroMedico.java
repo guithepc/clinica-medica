094f9356d1b5933c68b66c91b43ff91c8adeb24d
Guilherme <guilherme.pc@yahoo.com>
2023-03-30 21:38:21 -0300

    first commit

---
+++ src/main/java/com/example/demo/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package com.example.demo.medico;
+
+import com.example.demo.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
