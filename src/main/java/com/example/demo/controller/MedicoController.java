efbf0ecd10cf45d106a58100ddc0b1190fcf60b1
Guilherme <guilherme.pc@yahoo.com>
2023-04-04 00:15:09 -0300

    adding DTO Validations and POST method

--- src/main/java/com/example/demo/controller/MedicoController.java
+++ src/main/java/com/example/demo/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.medico.DadosCadastroMedico;
+import com.example.demo.medico.Medico;
+import com.example.demo.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @Transactional
     @PostMapping
-    public void cadastrar (@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    public void cadastrar (@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
 
     }
 }
--- src/main/java/com/example/demo/medico/Medico.java
+++ src/main/java/com/example/demo/medico/Medico.java
@@ -25,4 +25,12 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.crm = dados.crm();
+        this.email = dados.email();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
